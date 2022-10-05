b04321858fbe5a4dcc4d149d5e4b6725d5c07b42
Mirismoil <yuldashevmirismoil3@gmail.com>
2022-10-05 18:55:51 +0500

    addit

--- js/database.js
+++ js/database.js
@@ -1,17 +1,12 @@
 export const data = [
   {
     id: 1,
-    login: "Ali",
-    password: 123,
+    login: "Mirismoil",
+    password: 7766,
   },
   {
     id: 2,
-    login: "Vali",
-    password: 1234,
-  },
-  {
-    id: 3,
-    login: "Salim",
-    password: 12345,
+    login: "Tilla",
+    password: 7766,
   },
 ];
